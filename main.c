0ba849bf07b9730df6ad5f5db064d0532d2d683c
Meng Guang <mengguang@gmail.com>
2021-07-05 04:46:38 +0800

    new test on 2021

--- main.c
+++ main.c
@@ -10,17 +10,20 @@ uint32_t ndev = 0;
 
 FT_HANDLE ftHandle = NULL;
 
-void ListFtUsbDevices() {
+void ListFtUsbDevices()
+{
     FT_STATUS ftStatus = 0;
 
     DWORD numOfDevices = 0;
     ftStatus = FT_CreateDeviceInfoList(&numOfDevices);
-    if (ftStatus != FT_OK) {
+    if (ftStatus != FT_OK)
+    {
         printf("FT_CreateDeviceInfoList error: %lu\n", ftStatus);
         return;
     }
 
-    for (DWORD iDev = 0; iDev < numOfDevices; ++iDev) {
+    for (DWORD iDev = 0; iDev < numOfDevices; ++iDev)
+    {
         FT_DEVICE_LIST_INFO_NODE devInfo;
         memset(&devInfo, 0, sizeof(devInfo));
 
@@ -30,9 +33,11 @@ void ListFtUsbDevices() {
                                           devInfo.Description,
                                           &devInfo.ftHandle);
 
-        if (FT_OK == ftStatus) {
+        if (FT_OK == ftStatus)
+        {
             const char *desc = devInfo.Description;
-            if ((strcmp(desc, "FT4222") == 0) || (strcmp(desc, "FT4222 A") == 0)) {
+            if ((strcmp(desc, "FT4222") == 0) || (strcmp(desc, "FT4222 A") == 0))
+            {
                 g_FT4222DevList[ndev] = devInfo;
                 ndev++;
             }
@@ -40,25 +45,33 @@ void ListFtUsbDevices() {
     }
 }
 
-
-char *DeviceFlagToString(DWORD flags) {
+char *DeviceFlagToString(DWORD flags)
+{
     static char desc[32];
-    if (flags & 0x01) {
+    if (flags & 0x01)
+    {
         snprintf(desc, 32, "%s, ", "DEVICE_OPEN");
-    } else {
+    }
+    else
+    {
         snprintf(desc, 32, "%s, ", "DEVICE_CLOSED");
     }
-    if (flags & 0x02) {
+    if (flags & 0x02)
+    {
         snprintf(desc + strlen(desc), 32 - strlen(desc), "%s", "High-speed USB");
-    } else {
+    }
+    else
+    {
         snprintf(desc + strlen(desc), 32 - strlen(desc), "%s", "Full-speed USB");
     }
     return desc;
 }
 
-int main() {
+int main()
+{
     ListFtUsbDevices();
-    if (ndev == 0) {
+    if (ndev == 0)
+    {
         printf("No FT4222 device is found!\n");
         return 0;
     }
@@ -73,11 +86,11 @@ int main() {
     printf("  Description = %s\n", devInfo.Description);
     printf("  ftHandle = 0x%p\n", devInfo.ftHandle);
 
-
     FT_STATUS ftStatus;
     ftStatus = FT_OpenEx((PVOID)((int64_t)(devInfo.LocId)), FT_OPEN_BY_LOCATION, &ftHandle);
     //ftStatus = FT_Open(0, &ftHandle);
-    if (FT_OK != ftStatus) {
+    if (FT_OK != ftStatus)
+    {
         printf("Open a FT4222 device failed!\n");
         return 0;
     }
@@ -85,16 +98,18 @@ int main() {
     printf("\n\n");
     printf("Init FT4222 as I2C master\n");
     ftStatus = FT4222_I2CMaster_Init(ftHandle, 1000);
-    if (FT_OK != ftStatus) {
+    if (FT_OK != ftStatus)
+    {
         printf("Init FT4222 as I2C master device failed!\n");
         return 0;
     }
     FT4222_I2CMaster_Reset(ftHandle);
 
     ssd1306_Init();
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 1000; i++)
+    {
+        printf("testing %d\n", i);
         ssd1306_TestAll();
-        printf("testing %d\n",i);
     }
 
     printf("UnInitialize FT4222\n");
